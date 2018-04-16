2471b628627c0021bde84167fdbefc7beda6f9b4
Fahad Qazi <fahad@blueeye>
2018-04-16 16:52:45 +0100

    adding code to add user

--- install.sh
+++ install.sh
@@ -4,6 +4,28 @@
 # Script to set up worker          #
 ####################################
 
+USERNAME=origin-task-worker
+
+function add_new_user(){
+    if [ "$UID" -ne "$ROOT_UID" ]
+    then
+        echo "Must be root user to run this script"
+        exit 1
+    else
+        grep -q "whoami" /etc/passwd
+        if [ $? -eq $SUCCESS ]
+        then
+            echo "User $whoami already exists"
+            exit 1
+        else 
+            sudo useradd --no-user-group --gid www-data --system --create-home \
+                --shell /usr/sbin/nologin $USERNAME
+            echo "Setting up Account"
+        fi
+    fi
+
+}
+
 
 function already_installed(){
     if [ $(which $1) ]
