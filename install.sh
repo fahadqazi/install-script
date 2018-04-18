da2f7e26ca087b22d46d43dbcf457b4d78b4bc06
Fahad Qazi <fahad@blueeye>
2018-04-18 15:59:04 +0100

    pm2 & node install check

--- install.sh
+++ install.sh
@@ -14,12 +14,9 @@ function program_is_installed(){
 }
 
 function npm_package_is_installed(){
-
     local return_=1
-
-    ls node_modules | grep $1 >/dev/null 2>&1 || { local return_= 0; }
+    type $1 >/dev/null 2>&1 || { local return_=0; }
     echo "$return_"
-
 }
 
 function add_new_user(){
@@ -39,7 +36,6 @@ function add_new_user(){
             echo "Setting up Account"
         fi
     fi
-
 }
 
 
@@ -76,59 +72,37 @@ function install_curl(){
 }
 
 function install_node(){
-    #already_installed nodejs
-    program_is_installed nodejs
-    res=$?
-    echo "comes back $?"
-    echo "This is the result $res"
-    if [ "$res" -eq 0 ]
+    res=$(program_is_installed nodejs)
+    echo "node res is $res"
+    if [ "$res" -eq 1 ]
     then 
         echo
         echo ">>>" Node already installed
     else
-#        echo
-#        echo Installing Node Source
-        #curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
-
-#        echo
-#        echo Running Node Source script
-        #bash nodesource_setup.sh
-
-#        echo
         echo Installing NodeJS
-        #apt-get install nodejs
 #        apt-get update
 #        apt-get install nodejs
 #        apt-get install npm
-
     fi
 
 }
 
-function install_build_essentials(){
-    echo
-    echo 'Installing Build Essentials'
-    #apt-get install build-essential
-}
-
 function install_pm2(){
-    #already_installed pm2
-    npm_package_is_installed pm2
+    res=$(npm_package_is_installed pm2)
 
-    if [ $? -eq 0 ]
+    echo "pm2 res is $res"
+    if [ "$res" -eq 1 ]
     then
         echo
         echo ">>>" PM2 is already installed
     else
         echo
         echo Intalling PM2
-        npm i -g pm2
+#        npm i -g pm2
     fi
-
 }
 
-#update_upgrade
-#install_curl
+update_upgrade
+install_curl
 install_node
-#install_build_essentials
-#install_pm2
+install_pm2
